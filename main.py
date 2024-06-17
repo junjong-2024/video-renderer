ad54961959e2d3a2901cf4bd6742e774e003d38c
BasicNeural <basicneural@gmail.com>
2024-06-17 23:15:17 +0900

    wip

--- main.py
+++ main.py
@@ -17,9 +17,12 @@ def main():
     channel.queue_declare(queue='render')
 
     def callback(ch, method, properties, body):
-        data = json.loads(body)
-        print_log(data)
-        render_ffmpeg.render(data)
+        try:
+            data = json.loads(body)
+            print_log(data)
+            render_ffmpeg.render(data)
+        except Exception as ex:
+            print_log('Error caused:', ex)
 
     channel.basic_consume(queue='render', on_message_callback=callback, auto_ack=True)
 
